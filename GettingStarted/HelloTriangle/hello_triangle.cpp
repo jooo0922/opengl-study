7563ac372d21ebb95e5e399f42a74e0cb8331d54
jooo0922 <jooo0922@gmail.com>
2023-08-06 00:56:38 +0900

    feat: glad 및 glfw 라이브러리 포함 후, 프로젝트 빌드 테스트

---
+++ GettingStarted/HelloTriangle/hello_triangle.cpp
@@ -0,0 +1,9 @@
+#include <glad/glad.h> // 운영체제(플랫폼)별 OpenGL 함수를 함수 포인터에 저장 및 초기화 (OpenGL 을 사용하는 다른 라이브러리(GLFW)보다 먼저 include 할 것.)
+#include <GLFW/glfw3.h> // OpenGL 컨텍스트 생성, 윈도우 생성, 사용자 입력 처리 관련 OpenGL 라이브러리
+
+#include <iostream>
+
+int main()
+{
+	return 0;
+}
