e7aca65e28f6a908868e7ec4aaef6d05a540dc33
jooo0922 <jooo0922@gmail.com>
2023-08-17 22:34:21 +0900

    feat: 버텍스 위치 attribute 로 색상 보간

--- GettingStarted/Shaders_Quiz_3/MyShaders/shader.fs
+++ GettingStarted/Shaders_Quiz_3/MyShaders/shader.fs
@@ -3,7 +3,20 @@
 out vec4 FragColor;
 
 in vec3 ourColor;
+in vec3 ourPos;
 
 void main() {
-  FragColor = vec4(ourColor, 1.0);
+  FragColor = vec4(ourPos, 1.0);
 }
+
+/*
+  삼각형 왼쪽 하단이 검정색으로 보이는 이유
+
+  삼각형 왼쪽 하단의 위치 attribute 변수(aPos)는
+  vec3(-0.5, -0.5, 0.0) 으로 넘어옴.
+
+  이때, glsl 은 색상값이 음수일 때 0.0 으로 clamping(잘라내기) 함.
+
+  따라서, FragColor 최종 출력 변수에 들어가는 색상값이
+  vec3(0.0, 0.0, 0.0, 1.0) 이 되어서 검정색으로 보이는 것.
+*/
