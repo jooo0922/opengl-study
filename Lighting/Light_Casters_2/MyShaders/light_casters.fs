534dc3e5f91f29672eba8878f60923ae15f24f70
jooo0922 <jooo0922@gmail.com>
2023-10-19 23:17:52 +0900

    feat: 감쇄 계산 및 조명 성분에 적용

--- Lighting/Light_Casters_2/MyShaders/light_casters.fs
+++ Lighting/Light_Casters_2/MyShaders/light_casters.fs
@@ -10,19 +10,19 @@ struct Material {
   float shininess;
 };
 
-/* Directional Light 구조체 선언 */
+/* Point Light 구조체 선언 */
 // Light 구조체 선언 (조명 위치 및 색상)
 struct Light {
-  // vec3 position; // Directional Light 는 광원의 위치를 기준으로 조명벡터를 계산하지 않음!
-
-   // Directional Light 는 광원의 위치와 무관하게 모든 프래그먼트에 평행한 방향으로 입사하는 조명을 계산하므로, 
-   // 모든 조명벡터에 대해 '동일한 방향벡터'를 사용함.
-   // 이때, 광원에서 프래그먼트 방향으로 입사되는 방향벡터를 입력받기 때문에, 실제 조명계산 시, negate 해줘야 함!
-  vec3 direction;
+  vec3 position; // Point Light 광원 위치
 
   vec3 ambient;
   vec3 diffuse;
   vec3 specular;
+
+  // 감쇄 계산에 사용할 상수값 선언
+  float constant; // Kc
+  float linear; // Kl (linear term 상수)
+  float quadratic; // Kq (quadratic term 상수)
 };
 
 // 버텍스 쉐이더에서 전송된 보간 변수
@@ -42,7 +42,7 @@ void main() {
   /* diffuse 성분 계산 */
   // 조명계산에 사용되는 모든 방향벡터들은 항상 정규화를 해줄 것! -> 그래야 내적계산 시 정확한 cos 값만 얻을 수 있음!
   vec3 norm = normalize(Normal); // 프래그먼트에 수직인 노멀벡터
-  vec3 lightDir = normalize(-light.direction); // 조명벡터 (Directional Light 구조체에 정의된 동일한 방향으로 평행하게 입사하는 방향벡터)
+  vec3 lightDir = normalize(light.position - FragPos); // 조명벡터
   float diff = max(dot(norm, lightDir), 0.0); // 노멀벡터와 조명벡터 내적 > diffuse 성분의 세기(조도) 계산 (참고로, 음수인 diffuse 값은 조명값 계산을 부정확하게 만들기 때문에, 0.0 으로 clamping 시킴)
   vec3 diffuse = light.diffuse * (diff * texture2D(material.diffsue, TexCoords).rgb); // diffuse 조명 색상 * (diffuse 조도 * 물체가 diffuse 에 대해 반사하는 색상) 으로 최종 diffuse 성분값 계산
 
@@ -51,6 +51,15 @@ void main() {
   vec3 reflectDir = reflect(-lightDir, norm); // 반사 벡터 (조명벡터는 카메라 위치부터 출발하도록 방향을 negate)
   float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess); // 뷰 벡터와 반사 벡터의 내적값을 32제곱 > 32는 shininess 값으로써, 값이 클수록 highlight 영역이 정반사되고, 값이 작을수록 난반사됨. > specular 조도 계산
   vec3 specular = light.specular * (spec * texture2D(material.specular, TexCoords).rgb); // specular 조명색상 * (specular 조도 * 물체가 specular 에 대해 반사하는 색상) 으로 specular 성분값 계산
+
+  /* attenuation (감쇄) 계산 */
+  float distance = length(light.position - FragPos); // Point Light 광원에서 각 프래그먼트 사이의 거리
+  float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance)); // 거리에 따라 지수적으로 감소하는 감쇄값 계산
+
+  // 계산해 둔 조명 성분에 감쇄 적용
+  ambient *= attenuation;
+  diffuse *= attenuation;
+  specular *= attenuation;
 
   // 3가지 성분을 모두 더한 조명 색상에 물체 색상을 component-wise 곱으로 계산하여 최종 색상 결정
   vec3 result = ambient + diffuse + specular;
@@ -65,4 +74,17 @@ void main() {
 
   lightColor - (objectColor * lightColor) 값이 
   물체가 흡수하는 빛의 양이 나온다고 함.
+*/
+
+/*
+  attenuation (감쇄)
+
+  점 광원에서 멀어질수록 지수적으로 감소하는 조명을 계산하기 위한 값
+  
+  분자는 1이고,
+  분모는 아래 세 개의 항으로 이루어져 있음.
+
+  1. constant term (Kc) : 분모가 1보다 작아지는 것을 방지하기 위해 더해주는 상수항
+  2. linear term (Kl * d) : 거리에 따라 선형적으로 증가하는 값 -> 이 값이 클수록 감쇄의 선형성이 증가
+  3. quadratic term (Kq * (d * d)) : 거리에 따라 지수적으로 증가하는 값 -> 이 값이 클수록 감쇄가 이차함수 그래프에 가까워 짐.
 */
