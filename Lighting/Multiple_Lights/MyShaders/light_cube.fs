b1de5934eedf9cc71f2b31dd21efde451bdc4ecb
jooo0922 <jooo0922@gmail.com>
2023-10-22 14:42:44 +0900

    feat: 광원 큐브에 DESERT 배경 point light 색상 적용

--- Lighting/Multiple_Lights/MyShaders/light_cube.fs
+++ Lighting/Multiple_Lights/MyShaders/light_cube.fs
@@ -2,7 +2,9 @@
 
 out vec4 FragColor;
 
+uniform vec3 lightCubeColor;
+
 void main() {
   // 광원 색상은 빛을 받는 큐브의 조명 색상 계산에 영향을 받지 않도록, 별도의 쉐이더에 상수인 흰색(vec4(1.0))값을 할당함.
-  FragColor = vec4(1.0);
+  FragColor = vec4(lightCubeColor, 1.0);
 }
