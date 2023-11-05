d9cfe39854893a3cff0668204442a2c34e6fc987
jooo0922 <jooo0922@gmail.com>
2023-11-05 23:13:14 +0900

    feat: 깊이 버퍼 시각화

--- AdvancedOpenGL/Depth_Testing/MyShaders/depth_testing.fs
+++ AdvancedOpenGL/Depth_Testing/MyShaders/depth_testing.fs
@@ -8,5 +8,8 @@ uniform sampler2D texture1;
 
 void main() {
   // 전송받은 텍스쳐 객체를 샘플링하여 최종 색상 계산
-  FragColor = texture2D(texture1, TexCoords);
+  // FragColor = texture2D(texture1, TexCoords);
+
+  // gl_FragCoord 에 들어있는 현재 프래그먼트의 깊이값을 최종 색상으로 출력하여 depth buffer 시각화
+  FragColor = vec4(vec3(gl_FragCoord.z), 1.0);
 }
