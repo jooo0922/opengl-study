c5e7f4c003daa762ce91a75faee044ffab37873b
jooo0922 <jooo0922@gmail.com>
2023-11-05 22:11:40 +0900

    feat: 깊이 테스트 연산 모드를 GL_ALWAYS 로 변경

--- AdvancedOpenGL/Depth_Testing/depth_testing.cpp
+++ AdvancedOpenGL/Depth_Testing/depth_testing.cpp
@@ -91,6 +91,11 @@ int main()
 	// OpenGL 컨텍스트의 state-setting 함수 중 하나겠지! 
 	glEnable(GL_DEPTH_TEST);
 
+	// depth buffer 에 저장된 깊이값과 현재 프래그먼트의 깊이값을 비교(= 깊이 테스팅)하는 비교 연산 모드를
+	// glDepthFunc() 함수를 사용하여 변경할 수 있음!
+	// 각 깊이값 비교 연산 모드 관련 https://learnopengl.com/Advanced-OpenGL/Depth-testing 참고
+	glDepthFunc(GL_ALWAYS); // GL_ALWAYS 는 모든 프래그먼트 깊이값을 통과시킴 -> 깊이 테스트를 안하는 것과 마찬가지
+
 	// Shader 클래스를 생성함으로써, 쉐이더 객체 / 프로그램 객체 생성 및 컴파일 / 링킹
 	Shader shader("MyShaders/depth_testing.vs", "MyShaders/depth_testing.fs");
 
