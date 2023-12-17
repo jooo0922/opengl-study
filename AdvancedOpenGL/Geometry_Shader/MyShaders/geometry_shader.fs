5c64b5a3d010208988513cd8bd638a02e26eb6bd
jooo0922 <jooo0922@gmail.com>
2023-12-18 01:05:53 +0900

    feat: 지오메트리 쉐이더에서 전달받은 색상값 출력

--- AdvancedOpenGL/Geometry_Shader/MyShaders/geometry_shader.fs
+++ AdvancedOpenGL/Geometry_Shader/MyShaders/geometry_shader.fs
@@ -2,7 +2,10 @@
 
 out vec4 FragColor;
 
+// 지오메트리 쉐이더 단계에서 전달받은 각 정점으로부터 보간된 색상 데이터
+in vec3 fColor;
+
 void main() {
-  // 초록색을 최종 색상으로 출력
-  FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+  // 지오메트리 쉐이더에서 전달받아 보간된 색상을 최종 색상으로 출력
+  FragColor = vec4(fColor, 1.0);
 }
