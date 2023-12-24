d0434350088b4e09b8518471e4451e0dd1b0b5e7
jooo0922 <jooo0922@gmail.com>
2023-12-25 02:45:10 +0900

    feat: 프래그먼트 쉐이더에 색상 입력변수 선언

--- AdvancedOpenGL/Instancing/MyShaders/instancing.fs
+++ AdvancedOpenGL/Instancing/MyShaders/instancing.fs
@@ -2,7 +2,10 @@
 
 out vec4 FragColor;
 
+// 버텍스 쉐이더 단계에서 보간되는 색상값 입력변수 
+in vec3 fColor;
+
 void main() {
   // 빨간색을 최종 색상으로 출력
-  FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  FragColor = vec4(fColor, 1.0);
 }
