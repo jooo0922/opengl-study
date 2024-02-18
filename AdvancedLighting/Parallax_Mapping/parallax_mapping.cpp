fb259f18c1295423df1a4128565029143cbb5f6f
jooo0922 <jooo0922@gmail.com>
2024-02-19 01:20:01 +0900

    rename: 쉐이더 파일 이름 변경

--- AdvancedLighting/Parallax_Mapping/parallax_mapping.cpp
+++ AdvancedLighting/Parallax_Mapping/parallax_mapping.cpp
@@ -128,7 +128,7 @@ int main()
 	glEnable(GL_DEPTH_TEST);
 
 	// 큐브 렌더링 시 적용할 쉐이더 객체 생성
-	Shader shader("MyShaders/normal_mapping.vs", "MyShaders/normal_mapping.fs");
+	Shader shader("MyShaders/parallax_mapping.vs", "MyShaders/parallax_mapping.fs");
 
 
 	/* 텍스쳐 객체 생성 및 쉐이더 프로그램 전송 */
