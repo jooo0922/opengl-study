67be52ac43febcadd3f203b6197cf1208d58335e
jooo0922 <jooo0922@gmail.com>
2024-03-20 00:29:42 +0900

    feat: 각 G-buffer 에 geometry data 저장

--- AdvancedLighting/Deferred_Shading/MyShaders/g_buffer.fs
+++ AdvancedLighting/Deferred_Shading/MyShaders/g_buffer.fs
@@ -1,50 +1,30 @@
 #version 330 core
 
-out vec4 FragColor;
+// layout location specifier 로 MRT 프레임버퍼에 바인딩된 각 color attachment(G-buffer) 에 대응되는 출력 변수 선언
+// ex> location = 0 으로 지정 시, 해당 출력 변수에 입력되는 색상은 GL_COLOR_ATTACHMENT0 버퍼에 저장됨.
+layout(location = 0) out vec3 gPosition;
+layout(location = 1) out vec3 gNormal;
+layout(location = 2) out vec4 gAlbedoSpec;
 
-// 버텍스 쉐이더에서 전송받은 텍스쳐 좌표 입력변수 선언
+/* vertex shader 단계에서 전달받는 입력 변수 선언 */
+in vec3 FragPos;
+in vec3 Normal;
 in vec2 TexCoords;
 
-/* uniform 변수 선언 */
-
-// image 텍스쳐 (blur 처리를 적용할 텍스쳐 객체(= color attachment) 색상 버퍼)
-uniform sampler2D image;
-
-// blur 처리 방향 변수
-uniform bool horizontal;
-
-// 각 blur 방향으로 샘플링된 texel 에 적용할 가중치 kernel (= convolution matrix)
-uniform float weight[5] = float[](0.2270270270, 0.1945945946, 0.1216216216, 0.0540540541, 0.0162162162);
+// Assimp 를 추상화한 Model 클래스에서 전송한 텍스쳐 객체의 sampler 변수 선언
+uniform sampler2D texture_diffuse1;
+uniform sampler2D texture_specular1;
 
 void main() {
-  // 각 blur 방향의 주변 texel 좌표 계산을 위해 더해줄 offset 값 -> 단일 texel 사이즈에 해당
-  vec2 tex_offset = 1.0 / textureSize(image, 0);
+  // gPosition 텍스쳐 버퍼에는 프래그먼트의 월드 공간 위치값 저장 
+  gPosition = FragPos;
 
-  // 현재 texel 을 중심으로 가중치를 적용하여 누산할 변수 result 선언 및 초기화
-  // blur 를 적용할 현재 texel (= kernel 의 중점) 에 첫 번째 가중치를 곱하여 누산값 초기화 
-  vec3 result = texture(image, TexCoords).rgb * weight[0];
+  // gNormal 텍스쳐 버퍼에는 프래그먼트의 월드 공간 노멀벡터 저장
+  gNormal = normalize(Normal);
 
-  // 현재 texel 을 중심으로, blur 처리 방향(= horizontal)을 따라 4개(5개에서 현재 texel 1개 제외)의 주변 texel 을 샘플링하여 가중치 적용 및 누산
-  if(horizontal) {
-    // 수평 방향 blur 처리
-    for(int i = 1; i < 5; i++) {
-      // 현재 texel 의 오른쪽 방향 주변 texel 들에 대해 가중치 적용 및 누산
-      result += texture(image, TexCoords + vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
+  // gAlbedoSpec 텍스쳐 버퍼의 .rgb 성분에는 diffuse texture 에서 샘플링한 색상값 저장
+  gAlbedoSpec.rgb = texture2D(texture_diffuse1, TexCoords).rgb;
 
-      // 현재 texel 의 왼쪽 방향 주변 texel 들에 대해 가중치 적용 및 누산
-      result += texture(image, TexCoords - vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
-    }
-  } else {
-    // 수직 방향 blur 처리
-    for(int i = 1; i < 5; i++) {
-      // 현재 texel 의 위쪽 방향 주변 texel 들에 대해 가중치 적용 및 누산
-      result += texture(image, TexCoords + vec2(0.0, tex_offset.y * i)).rgb * weight[i];
-
-      // 현재 texel 의 아래쪽 방향 주변 texel 들에 대해 가중치 적용 및 누산
-      result += texture(image, TexCoords - vec2(0.0, tex_offset.y * i)).rgb * weight[i];
-    }
-  }
-
-  // 누산된 최종 색상값을 출력 변수에 저장
-  FragColor = vec4(result, 1.0);
+  // gAlbedoSpec 텍스쳐 버퍼의 .a 성분에는 specular texture 에서 샘플링한 specular intensity 값 저장
+  gAlbedoSpec.a = texture2D(texture_specular1, TexCoords).a;
 }
