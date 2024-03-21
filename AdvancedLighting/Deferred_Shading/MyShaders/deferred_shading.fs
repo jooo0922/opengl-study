6f00b8cc839bdb64337b5b8080ebeb7f8a2e8f4c
jooo0922 <jooo0922@gmail.com>
2024-03-21 22:48:47 +0900

    feat: light volume 에 포함된 프래그먼트만 조명 연산 수행

--- AdvancedLighting/Deferred_Shading/MyShaders/deferred_shading.fs
+++ AdvancedLighting/Deferred_Shading/MyShaders/deferred_shading.fs
@@ -18,6 +18,7 @@ struct Light {
 
   float Linear; // 거리에 따른 조명 감쇄 계산식에서 사용할 Linear 항의 계수
   float Quadratic; // 거리에 따른 조명 감쇄 계산식에서 사용할 Quadratic 항의 계수
+  float Radius; // light volume 의 반경
 };
 
 /* 
@@ -59,41 +60,44 @@ void main() {
 
   // 각 조명을 순회하며 조명값 계산 및 누산
   for(int i = 0; i < NR_LIGHTS; i++) {
+    // 광원으로부터의 거리값 계산
+    float distance = length(lights[i].Position - FragPos);
+
+    // light volume 내에 들어오는 프래그먼트에 대해서만 조명 연산 수행 -> light volume 을 활용한 최적화!
+    if(distance < lights[i].Radius) {
     /* diffuse 성분값 계산 */
 
     // 조명벡터 (프래그먼트 위치 ~ 광원 위치)
-    vec3 lightDir = normalize(lights[i].Position - FragPos);
+      vec3 lightDir = normalize(lights[i].Position - FragPos);
 
     // 노멀벡터와 조명벡터 내적 > diffuse 성분의 세기(조도) 계산 (참고로, 음수인 diffuse 값은 조명값 계산을 부정확하게 만들기 때문에, 0.0 으로 clamping 시킴)
     // diffuse 성분값 * 모델의 원 색상(Diffuse) * 조명 색상
-    vec3 diffuse = max(dot(Normal, lightDir), 0.0) * Diffuse * lights[i].Color;
+      vec3 diffuse = max(dot(Normal, lightDir), 0.0) * Diffuse * lights[i].Color;
 
     /* specular 성분값 계산 */
 
     // blinn-phong half vector 계산
-    vec3 halfwayDir = normalize(lightDir + viewDir);
+      vec3 halfwayDir = normalize(lightDir + viewDir);
 
     // spec 성분값 계산 (shininess 16 거듭제곱)
-    float spec = pow(max(dot(Normal, halfwayDir), 0.0), 16.0);
+      float spec = pow(max(dot(Normal, halfwayDir), 0.0), 16.0);
 
     // 조명 색상 * specular 성분값 * specular intensity
-    vec3 specular = lights[i].Color * spec * Specular;
+      vec3 specular = lights[i].Color * spec * Specular;
 
     /* 거리에 따른 감쇄 계산 */
 
-    // 각 광원 ~ 프래그먼트 사이의 거리값 계산
-    float distance = length(lights[i].Position - FragPos);
-
     // 거리에 따른 감쇄량 계산
     // 감쇄 계산 공식 관련 https://github.com/jooo0922/opengl-study/blob/main/Lighting/Light_Casters_2/MyShaders/light_casters.fs 참고
-    float attenuation = 1.0 / (1.0 + lights[i].Linear * distance + lights[i].Quadratic * distance * distance);
+      float attenuation = 1.0 / (1.0 + lights[i].Linear * distance + lights[i].Quadratic * distance * distance);
 
     // diffuse 성분과 specular 성분 각각에 감쇄 적용
-    diffuse *= attenuation;
-    specular *= attenuation;
+      diffuse *= attenuation;
+      specular *= attenuation;
 
     // diffuse 성분값과 specular 성분값을 더해 최종 결과값에 누산
-    lighting += diffuse + specular;
+      lighting += diffuse + specular;
+    }
   }
 
   // FragColor 출력 변수에 누산된 최종 색상값 할당
