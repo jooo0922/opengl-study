470ee019907942c6a390a73eecba3f3ace3c7a1b
jooo0922 <jooo0922@gmail.com>
2024-05-05 01:25:34 +0900

    feat: PBR 텍스쳐 및 광원 정보 uniform 변수 선언

--- PBR/Direct_Lighting/MyShaders/pbr.fs
+++ PBR/Direct_Lighting/MyShaders/pbr.fs
@@ -2,18 +2,19 @@
 
 out vec4 FragColor;
 
-// vertex shader 단계에서 전달받는 입력 interface block 선언
-in VS_OUT {
-  vec3 FragPos;
-  vec3 Normal;
-  vec2 TexCoords;
-} fs_in;
+// vertex shader 단계에서 전달받는 입력 변수 선언
+in vec2 TexCoords;
+in vec3 WorldPos;
+in vec3 Normal;
 
-// OpenGL 에서 전송해 줄 uniform 변수들 선언
-uniform sampler2D floorTexture; // 바닥 평면 텍스쳐 (0번 texture unit 에 바인딩된 텍스쳐 객체 샘플링)
-uniform vec3 lightPos; // 광원 위치 > 조명벡터 계산에서 사용
-uniform vec3 viewPos; // 카메라 위치 > 뷰 벡터 계산에서 사용
-uniform bool blinn; // blinn-phong 반사 모델 적용 모드 상태값
+/* OpenGL 에서 전송해 줄 uniform 변수들 선언 */
+
+// PBR Material 파라미터 값이 텍셀 단위로 저장된 텍스쳐 객체들의 sampler 변수 선언
+uniform sampler2D albedoMap;
+uniform sampler2D normalMap;
+uniform sampler2D metallicMap;
+uniform sampler2D roughnessMap;
+uniform sampler2D aoMap;
 
 void main() {
   vec3 color = texture2D(floorTexture, fs_in.TexCoords).rgb;
