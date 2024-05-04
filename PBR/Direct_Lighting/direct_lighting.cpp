c9be46aee89a485af6b52a11902102cc0471bf0f
jooo0922 <jooo0922@gmail.com>
2024-05-04 14:06:22 +0900

    refactor: 바닥 평면 그리기 명령 호출 코드 제거

--- PBR/Direct_Lighting/direct_lighting.cpp
+++ PBR/Direct_Lighting/direct_lighting.cpp
@@ -205,19 +205,6 @@ int main()
 		shader.setInt("blinn", blinn);
 
 
-		/* 바닥 평면 그리기 */
-
-		// 바닥 평면에 적용할 VAO 객체를 바인딩하여, 해당 객체에 저장된 VBO 객체와 설정대로 그리도록 명령
-		glBindVertexArray(planeVAO);
-
-		// 이 예제에서는 모든 텍스쳐 객체가 0번 texture unit 을 공유할 것이므로, 0번 위치에 텍스쳐 객체가 바인딩되도록 활성화
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, albedo);
-
-		// 바닥 평면 그리기 명령
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-
-
 		// 현재 Blinn-Phong 조명 모델 적용 상태를 콘솔에 출력
 		std::cout << (blinn ? "Blinn-Phong" : "Phong") << std::endl;
 
@@ -228,10 +215,6 @@ int main()
 		// 키보드, 마우스 입력 이벤트 발생 검사 후 등록된 콜백함수 호출 + 이벤트 발생에 따른 GLFWwindow 상태 업데이트
 		glfwPollEvents();
 	}
-
-	// 렌더링 루프 종료 시, 생성해 둔 VAO, VBO 객체들은 더 이상 필요가 없으므로 메모리 해제한다!
-	glDeleteVertexArrays(1, &planeVAO);
-	glDeleteBuffers(1, &planeVBO);
 
 	// while 렌더링 루프 탈출 시, GLFWwindow 종료 및 리소스 메모리 해제
 	glfwTerminate();
