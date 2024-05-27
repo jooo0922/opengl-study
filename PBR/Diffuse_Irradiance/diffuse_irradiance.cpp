0f5541af47d4e63644a0c750a5e8262e17c40946
jooo0922 <jooo0922@gmail.com>
2024-05-28 00:49:53 +0900

    feat: .hdr 파일 로드 및 텍스쳐 객체 생성

--- PBR/Diffuse_Irradiance/diffuse_irradiance.cpp
+++ PBR/Diffuse_Irradiance/diffuse_irradiance.cpp
@@ -188,6 +188,53 @@ int main()
 	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, captureRBO);
 
 
+	/* stb_image 라이브러리로 .hdr 파일 로드하여 텍스쳐 객체 생성 */
+
+	// 텍스쳐 이미지 로드 후, y축 방향으로 뒤집어 줌 > OpenGL 이 텍스쳐 좌표를 읽는 방향과 이미지의 픽셀 좌표가 반대라서!
+	stbi_set_flip_vertically_on_load(true);
+
+	// 로드한 .hdr 이미지의 width, height, 색상 채널 개수를 저장할 변수 선언
+	int width, height, nrComponents;
+
+	// 이미지 데이터 가져와서 float 타입의 bytes 데이터로 저장. 
+	// 이미지 width, height, 색상 채널 변수의 주소값도 넘겨줌으로써, 해당 함수 내부에서 값을 변경. -> 출력변수 역할
+	float* data = stbi_loadf("resources/textures/hdr/newport_loft.hdr", &width, &height, &nrComponents, 0);
+
+	// 텍스쳐 객체(object) 참조 id 를 저장할 변수 선언
+	unsigned int hdrTexture;
+
+	if (data)
+	{
+		// 텍스쳐 객체 생성 및 바인딩
+		glGenTextures(1, &hdrTexture);
+		glBindTexture(GL_TEXTURE_2D, hdrTexture);
+
+		/*
+			[0, 1] 범위를 넘어선 HDR 이미지 데이터(data)들을 온전히 저장하기 위해, 			
+			GL_RGB16F floating point(부동 소수점) 포맷으로 프레임버퍼의 내부 색상 포맷 지정
+			(하단 Floating point framebuffer 관련 필기 참고)
+		*/
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, width, height, 0, GL_RGB, GL_FLOAT, data);
+
+		// 현재 GL_TEXTURE_2D 상태에 바인딩된 텍스쳐 객체 설정하기
+		// Texture Wrapping 모드를 반복 모드로 설정 ([(0, 0), (1, 1)] 범위를 벗어나는 텍스쳐 좌표에 대한 처리)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+		// 텍스쳐 축소/확대 및 Mipmap 교체 시 Texture Filtering (텍셀 필터링(보간)) 모드 설정
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		// 텍스쳐 객체에 이미지 데이터를 전달하고, 밉맵까지 생성 완료했다면, 로드한 이미지 데이터는 항상 메모리 해제할 것!
+		stbi_image_free(data);
+	}
+	else
+	{
+		// HDR 이미지 데이터 로드 실패 시 처리
+		std::cout << "Failed to load HDR image." << std::endl;
+	}
+
+
 	/*
 		투영행렬을 렌더링 루프 이전에 미리 계산
 		-> why? camera zoom-in/out 미적용 시, 투영행렬 재계산 불필요!
@@ -787,4 +834,82 @@ unsigned int loadTexture(const char* path)
 	필요한 VAO 객체를 교체하거나 꺼내쓸 수 있다.
 
 	즉, 저런 번거로운 VBO 객체 생성 및 설정 작업을 반복하지 않아도 된다는 뜻!
+*/
+
+/*
+	.hdr 파일이란 무엇인가?
+
+
+	PBR 파이프라인에서 계산되는 모든 값들은
+	실제 물리적인 측정치와 일치해야 더 정확하게 결과물을 렌더링할 수 있음!
+
+	그래서, 이전 PBR-Lighting 챕터에서도
+	Reflectance Equation 의 최종 결과값이 [0.0, 1.0] 사이로 clamping 되지 않도록,
+	HDR -> LDR 범위로 tone mapping 시킨 다음에 gl_FragColor 에 할당해줬었지?
+
+	이처럼, PBR 파이프라인 계산의 결과값이 clamping 되어
+	훼손되는 것을 방지하는 게 아주 중요한데,
+	IBL 에서도 이는 마찬가지임.
+
+	IBL 에서 사용되는 EnvMap 은 주변에서 오는 모든 간접광들에 대한
+	radiance 를 저장한 Cubemap 텍스쳐라고 보면 될텐데,
+
+	이러한 radiance 값이 일반적인 이미지 포맷으로 저장된 것으로부터 사용한다면,
+	LDR 범위([0.0, 1.0])로 clamping 된 데이터를 사용하게 되겠지?
+
+	이렇게 되면 당연히 부정확한 결과물이 나올 수밖에 없음.
+
+
+	그래서, 이러한 indirect light 들에 대한
+	radiance 를 HDR 범위([0.0, 1.0]을 넘어서는 범위)로 관리할 수 있도록,
+
+	각 픽셀을 floating point value(부동 소수점) 포맷으로 저장한 
+	특별한 이미지 파일을 .hdr, 즉, HDR file 이라고 하는 것임!
+
+
+	HDR 파일 포맷의 또 다른 특징이 있다면,
+	기존 cubemap 의 각 6면을 하나의 이미지 안에 저장해둘 수 있도록,
+	구체(sphere)를 평면(plane)에 투영한 등장방형도법('Equirectangular projection')으로
+	저장되어 있음.
+
+	그래서 겉으로 보기에는 약간 이미지가 왜곡되어 보이는 느낌이 들고,
+	대부분 가로 해상도가 더 넓기 때문에, cubemap 6면에서 top, bottom 면에 해당하는
+	영역의 픽셀 정보가 투영되는 과정에서 약간 유실된다는 단점이 있음.
+*/
+
+/*
+	Floating point framebuffer
+
+
+	HDR 을 구현하려면, [0, 1] 범위를 넘어서는 색상값들이
+	프레임버퍼에 attach 된 텍스쳐 객체에 저장될 때,
+	[0, 1] 사이로 clamping 되지 않고,
+
+	원래의 색상값이 그대로 저장될 수 있어야 함.
+
+	그러나, 일반적인 프레임버퍼에서 color 를 저장할 때,
+	내부 포맷으로 사용하는 GL_RGB 같은 포맷은
+	fixed point(고정 소수점) 포맷이기 때문에,
+
+	OpenGL 에서 프레임버퍼에 색상값을 저장하기 전에
+	자동으로 [0, 1] 사이의 값으로 clamping 해버리는 문제가 있음.
+
+
+	이를 해결하기 위해,
+	GL_RGB16F, GL_RGBA16F, GL_RGB32F, GL_RGBA32F 같은
+	floating point(부동 소수점) 포맷으로
+	프레임버퍼의 내부 색상 포맷을 변경하면,
+
+	[0, 1] 범위를 벗어나는 값들에 대해서도
+	부동 소수점 형태로 저장할 수 있도록 해줌!
+
+
+	이때, 일반적인 프레임버퍼의 기본 색상 포맷인
+	GL_RGB 같은 경우 하나의 컴포넌트 당 8 bits 메모리를 사용하는데,
+	GL_RGB32F, GL_RGBA32F 같은 포맷은 하나의 컴포넌트 당 32 bits 의 메모리를 사용하기 때문에,
+	우리는 이 정도로 많은 메모리를 필요로 하지는 않음.
+
+	따라서, GL_RGB16F, GL_RGBA16F 같이
+	한 컴포넌트 당 16 bits 정도의 메모리를 예약해서 사용하는
+	적당한 크기의 색상 포맷으로 사용하는 게 좋겠지!
 */
