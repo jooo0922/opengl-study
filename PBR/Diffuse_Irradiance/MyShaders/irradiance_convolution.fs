29a3de37cac15393155ca5c466166a519b39ef59
jooo0922 <jooo0922@gmail.com>
2024-06-03 23:42:21 +0900

    feat: tangent space 기저 축을 world space 기준으로 계산

--- PBR/Diffuse_Irradiance/MyShaders/irradiance_convolution.fs
+++ PBR/Diffuse_Irradiance/MyShaders/irradiance_convolution.fs
@@ -22,4 +22,27 @@ void main() {
   */
   vec3 N = normalize(WorldPos);
 
+  /* surface point P 지점으로 들어오는 irradiance 총량을 diffuse term 적분식으로 계산 */
+
+  // diffuse term 적분식을 이산적인(discrete) 리만 합(Riemann sum)으로 계산할 때, irradiance 결과값을 누산할 변수 초기화 (노션 IBL 관련 필기 정리 참고)
+  vec3 irradiance = vec3(0.0);
+
+  /*
+    surface point P 를 원점으로 하는 tangent space 기준으로 정의된
+    tangentSample 방향벡터를 world space 로 변환하기 위해,
+    
+    현재 tangent space 의 기저 축(Tangent, Bitangent, Normal)을
+    world space 로 변환하여 계산해 둠.
+
+    right -> world space 기준 Tangent 기저 축
+    up -> world space 기준 Bitangent 기저 축
+    N -> world space 기준 Normal 기저 축
+
+    이때, 이미 N 자체가 world space 기준으로 계산되어 있으므로,
+    world space 업 벡터인 up 과 외적하여 나머지 기저 축 또한 
+    world space 기준으로 계산할 수 있음!
+  */
+  vec3 up = vec3(0.0, 1.0, 0.0);
+  vec3 right = normalize(cross(up, N));
+  up = normalize(cross(N, right));
 }
