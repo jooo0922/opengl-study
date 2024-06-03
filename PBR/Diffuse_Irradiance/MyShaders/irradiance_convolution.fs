b8d7a2a6bfedcc00149a42b3c69a1bb91c2b54d7
jooo0922 <jooo0922@gmail.com>
2024-06-03 23:52:20 +0900

    feat: 반구 영역의 방위각과 고도각을 순회하는 이중 for-loop 구현

--- PBR/Diffuse_Irradiance/MyShaders/irradiance_convolution.fs
+++ PBR/Diffuse_Irradiance/MyShaders/irradiance_convolution.fs
@@ -45,4 +45,20 @@ void main() {
   vec3 up = vec3(0.0, 1.0, 0.0);
   vec3 right = normalize(cross(up, N));
   up = normalize(cross(N, right));
+
+  // 반구 영역의 고도각(polar azimuth)과 방위각(zenith angle)을 이산적으로(discretely) 순회할 각도 간격 정의 (노션 IBL 관련 필기 정리 참고)
+  // -> 이 간격이 작을수록 더 정확한 적분(리만 합(Riemann sum))을 계산할 수 있음. 즉, 더 정확한 irradiance 계산 가능
+  float sampleDelta = 0.025;
+
+  // LearnOpenGL 본문의 반구 영역의 고도각과 방위각에 대한 이중 시그마 식의 전체 항 개수를 누산해나갈 변수 초기화 -> 즉, 시그마 식의 n1n2 에 해당
+  float nrSamples = 0.0;
+
+  // 반구 영역의 방위각(zenith angle) 을 sampleDelta 간격으로 2PI(360도)까지 순회
+  for(float phi = 0; phi < 2.0 * PI; phi += sampleDelta) {
+
+    // 반구 영역의 고도각(polar azimuth) 을 sampleDelta 간격으로 PI / 2(90도)까지 순회
+    for(float theta = 0; theta < 0.5 * PI; theta += sampleDelta) {
+
+    }
+  }
 }
